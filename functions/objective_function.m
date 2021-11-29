9879106f504b1791ac399e87d1ed088b7fb12006
Jiaxi Ying <54426946+jxying@users.noreply.github.com>
2021-11-29 16:26:47 +0800

    Update objective_function.m

--- functions/objective_function.m
+++ functions/objective_function.m
@@ -1,10 +1,8 @@
 function F = objective_function(T,S)
-%% compute the objective function value
-
+% compute the objective function value
 [A, FLAG]=chol(T,'lower');
 lg_det =2*sum(log(diag(A)));
 fun = -1*lg_det + dot(T(:), S(:));
 F.value = fun;
 F.flag = FLAG;
 end
-
