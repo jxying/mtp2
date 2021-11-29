5c3788799429fd47632c57fe3dc56d4c17e9aa64
Jiaxi Ying <54426946+jxying@users.noreply.github.com>
2021-11-29 15:24:03 +0800

    Add files via upload

---
+++ demo.m
@@ -0,0 +1,46 @@
+%% Fast Projected Newton-like Method for Precision Matrix Estimation 
+%% with Nonnegative Partial Correlations
+  
+close all;
+clear;
+clc;
+%%
+addpath(genpath('functions'));
+addpath(genpath('data'));
+
+%% load synthetic data: BA model with degree one consisting of 1000 nodes
+data=load('BA1000.mat');
+Mtrue=data.Mtrue;
+
+%% set sample size
+p=size(Mtrue,1);
+n= p/2;
+
+%% Set the underlying multivariate Gaussian distribution
+Mu    = zeros(1,p);     % mean
+Sigma = inv(Mtrue);     % covariance matrix
+
+%% Generate sample covariance matrix
+Y = mvnrnd(Mu,Sigma,n);
+S = Y'*Y/n;
+
+%% Generate weighted regularization parameters
+alpha = 0.008;
+lmbd  = generate_weighted_lambda(S, alpha);
+
+%% Estimate precision matrices
+opts.max_iter = 1e3;
+opts.tol      = 1e-6;
+out           = solver_fpn(S, lmbd, opts);
+X_est         = out.X_est;  % estimated precision matrix
+run_time      = out.time;   % run time
+
+
+[Omega, Sigma, conv] = solver_bcd(S, lmbd, 1e-6, opts);
+
+
+
+
+
+
+
